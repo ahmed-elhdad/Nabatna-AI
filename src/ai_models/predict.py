2fa26f21b0031e57715ba6f0cf68423ad4e93ec4
Ahmed elhaddad <ahalhdad2023@gmail.com>
2026-05-08 06:25:42 +0300

    Refactor predict function to accept UploadFile and load model from settings

--- src/ai_models/predict.py
+++ src/ai_models/predict.py
@@ -1,6 +1,8 @@
-def predict(model):
-    predicted_labels=model.predict('../../data/plants/PlantVillage/Potato___Early_blight/00d8f10f-5038-4e0f-bb58-0b885ddc0cc5___RS_Early.B 8722.jpg')
+from fastapi import UploadFile
+import tensorflow as tf
+from src.helpers.config import get_settings,Settings
+settings=Settings()
+model=tf.keras.models.load_model(settings.TRAINED_MODEL_PATH)
+def predict(img:UploadFile):
+    predicted_labels=model.predict(img)
     return predicted_labels
-import joblib
-model=joblib.load('saved_models/model.h5')
-print(predict(model=model))
